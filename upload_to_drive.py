e79e719f63c26ef48e5562b08667e8c1bbbe8be4
Kamonchanok Butaka <157267645+Xuli2317@users.noreply.github.com>
2026-06-12 16:40:58 +0700

    fix file name

--- upload_to_drive.py
+++ upload_to_drive.py
@@ -36,11 +36,7 @@ def upload_file(local_path, drive_name=None):
     print(f"Uploaded {drive_name}: {result['id']}")
 
 
-for folder in ["data", "FPO", "BOT"]:
-    if not os.path.exists(folder):
-        continue
-
-    for root, dirs, files in os.walk(folder):
-        for filename in files:
-            local_path = os.path.join(root, filename)
-            upload_file(local_path)
+for root, dirs, files in os.walk("SENTIMENT_INDEX"):
+    for filename in files:
+        local_path = os.path.join(root, filename)
+        upload_file(local_path)
